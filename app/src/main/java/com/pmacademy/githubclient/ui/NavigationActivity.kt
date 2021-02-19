d523a085dc5744e30994c181e2cf4de310b6b542
Senix22 <senix2276@gmail.com>
2021-02-19 19:58:52 +0200

    added GitHubAuto

--- app/src/main/java/com/pmacademy/githubclient/ui/NavigationActivity.kt
+++ app/src/main/java/com/pmacademy/githubclient/ui/NavigationActivity.kt
@@ -2,65 +2,21 @@ package com.pmacademy.githubclient.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.pmacademy.githubclient.App
 import com.pmacademy.githubclient.R
-import com.pmacademy.githubclient.data.GithubAuthService
-import com.pmacademy.githubclient.data.models.AuthToken
-import com.pmacademy.githubclient.databinding.ActivityNavigationBinding
-import com.pmacademy.githubclient.utils.GithubUtils
-import com.pmacademy.githubclient.utils.SharedPref
-import javax.inject.Inject
 
 class NavigationActivity : AppCompatActivity() {
-
 
     val navigator: Navigator by lazy {
         Navigator(supportFragmentManager, R.id.container)
     }
-    private lateinit var binding: ActivityNavigationBinding
-
-    @Inject
-    lateinit var sharedPref: SharedPref
-
-    @Inject
-    lateinit var githubAuthService: GithubAuthService
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNavigationBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        (application as App).daggerComponent.inject(this)
+        setContentView(R.layout.activity_navigation)
     }
-
 
     override fun onResume() {
         super.onResume()
-        val code = GithubUtils.getCodeFromUri(intent.data)
-        if (code == null && sharedPref.accessToken == "") {
-            navigator.showAuthFragment()
-        } else {
-            code?.let { code ->
-                Thread {
-                    getAuthToken(code)?.let { authToken ->
-                        sharedPref.accessToken = authToken.accessToken
-                        sharedPref.refreshToken = authToken.refreshToken
-                        sharedPref.token_type = authToken.tokenType
-                        navigator.showUserInfoFragment()
-                    }
-                }.start()
-            }
-        }
-    }
-
-    private fun getAuthToken(code: String): AuthToken? {
-        return githubAuthService.getAccessToken(
-            GithubUtils.clientId,
-            GithubUtils.clientSecret,
-            code
-        )
-            .execute()
-            .body()
+        navigator.showAuthFragment()
     }
 }
